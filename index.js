efea4cbb47ba515f25c92ec4bdb14fa46b2ae81f
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-05 07:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(lineNumber,newPerson){
   lineNumber.push(newPerson);
   return "Welcome, " + newPerson + ". You are number " + lineNumber.length + " in line."
 }
+
+function nowServing(lineNumber){
+  if (lineNumber.length === 0){
+    return "There is nobody waiting to be served!";
+  }else{
+    return "Currently serving" +  lineNumber[0];
+  }
+}
