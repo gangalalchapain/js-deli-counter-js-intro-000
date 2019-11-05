b0572a564c456026e540414eef7dfedab4985899
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-05 06:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function takeANumber(katzDeliLine, name){
   var result = "";
   for (var i = 0; i < katzDeliLine.length; i++){
-    result += "Welcome, " + name[i] + "." +  "You are number " + katzDeliLine[i] +  " in line.";
+    result += "Welcome, " + name[i] + "." +  " You are number " + katzDeliLine[i] +  " in line.";
 
   }return result;
+
+
 }
